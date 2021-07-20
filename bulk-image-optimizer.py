70d7387e3fd9c5aaa1f10857ddba1465550660ab
Carlos Zambrana <carlosz@nicasource.com>
2021-07-20 15:53:04 -0600

    * Fixed a bug creating a file instead a folder * Refactor some code * added QUALITY and TOTAL_FILES as global variables * added more filetypes to .gitignore

--- bulk-image-optimizer.py
+++ bulk-image-optimizer.py
@@ -2,11 +2,16 @@ import os
 import subprocess
 from pathlib import Path
 from PIL import Image
+import errno
+import time
+from re import search
 
 CONVERT_PNG_TO_JPG = False
 TOTAL_ORIGINAL = 0
 TOTAL_COMPRESSED = 0
 TOTAL_GAIN = 0
+TOTAL_FILES = 0
+QUALITY = 85
 
 
 def compress(location):
@@ -18,12 +23,15 @@ def compress(location):
             path = location
             input_path = path + os.sep + image
             out_path = path.replace(r'input', r'output')
-            if image.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            if image.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', 'webp')):
                 if os.path.isfile(input_path):
                     global TOTAL_GAIN
                     global TOTAL_ORIGINAL
                     global TOTAL_COMPRESSED
+                    global TOTAL_FILES
+                    global QUALITY
                     opt = None
+                    
                     try:
                         opt = Image.open(input_path)
                     except:
@@ -35,28 +43,62 @@ def compress(location):
                     TOTAL_ORIGINAL += original_size
                     print(input_path)
                     print("Original size: " + f'{original_size:,.2f}' + " megabytes")
-                    Path(out_path).mkdir(parents=True, exist_ok=True)
-                    out_path = out_path + os.sep + image
+                    if not os.path.exists(out_path):
+                        try:
+                            os.makedirs(out_path, exist_ok=True)
+                        except OSError as e:
+                            #wait for race condition to settle
+                            time.sleep(1)
+                            # try to create the folder again
+                            os.makedirs(out_path, exist_ok=True)
+                            if e.errno != errno.EEXIST:
+                                raise
+                    
+                    out_file= out_path + os.sep + image
                     # Convert .pgn to .jpg
                     if CONVERT_PNG_TO_JPG and image.lower().endswith('.png'):
                         im = opt
                         rgb_im = im.convert('RGB')
-                        out_path = out_path.replace(".png", ".jpg")
-                        rgb_im.save(out_path)
-                        opt = Image.open(out_path)
-                    opt.save(out_path, optimize=True, quality=85)
-                    opt = Image.open(out_path)
-                    compressed_size = os.stat(out_path).st_size / 1024 / 1024
+                        out_file = out_file.replace(".png", ".jpg")
+                        rgb_im.save(out_file)
+                        opt = Image.open(out_file)
+                    opt.save(out_file, optimize=True, quality=QUALITY)
+                    opt = Image.open(out_file)
+                    compressed_size = os.stat(out_file).st_size / 1024 / 1024
                     TOTAL_COMPRESSED += compressed_size
                     gain = original_size - compressed_size
                     TOTAL_GAIN += gain
+                    TOTAL_FILES +=1
                     print("Compressed size: " + f'{compressed_size:,.2f}' + " megabytes")
                     print("Gain : " + f'{gain:,.2f}' + " megabytes")
                     opt.close()
             else:
+                if os.path.isdir(out_path) and not os.path.exists(out_path):
+                    try:
+                        os.makedirs(out_path, exist_ok=True)
+                    except OSError as e:
+                        #wait for race condition to settle
+                        time.sleep(1)
+                        # try to create the folder again
+                        os.makedirs(out_path, exist_ok=True)
+                        if e.errno != errno.EEXIST:
+                            raise
                 if os.path.isfile(input_path):
+                    
+                    if  not os.path.exists(out_path):
+                        try:
+                            os.makedirs(out_path, exist_ok=True)
+                        except OSError as e:
+                            #wait for race condition to settle
+                            time.sleep(1)
+                            # try to create the folder again
+                            os.makedirs(out_path, exist_ok=True)
+                            if e.errno != errno.EEXIST:
+                                raise        
+                    input_file = input_path
+                    output_file= input_file.replace('input','outout')        
                     print('File not image, copying instead: ' + input_path)
-                    subprocess.call('cp ' + input_path + ' ' + out_path, shell=True)
+                    subprocess.call('cp ' + input_file + ' ' + output_file, shell=True)
 
 
 if __name__ == '__main__':
@@ -68,6 +110,7 @@ if __name__ == '__main__':
     compress(start_path)
     print("---------------------------------------------------------------------------------------------")
     print('-------------------------------------------SUMMARY-------------------------------------------')
+    print('Files: '  + f'{TOTAL_FILES}')
     print(
         "Original: " + f'{TOTAL_ORIGINAL:,.2f}' + " megabytes || " + "New Size: " + f'{TOTAL_COMPRESSED:,.2f}' +
         " megabytes" + " || Gain: " + f'{TOTAL_GAIN:,.2f}' + " megabytes ~" + f'{(TOTAL_GAIN / TOTAL_ORIGINAL) * 100:,.2f}'
